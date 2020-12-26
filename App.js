5cfc93e4669e542ceeafa31c02d65a9a17e8b006
AjithArun2001 <ajitharu201@gmail.com>
2020-12-26 15:07:04 +0530

    changes

---
+++ App.js
@@ -0,0 +1,8 @@
+
+function myFunction()
+{
+    console.log("hi");
+    document.getElementById('full').style.cssText = 'display:none;';
+    document.getElementById('sky').style.cssText = 'display:unset;';
+    document.getElementById('rocket').style.cssText = 'animation-name: launch';
+}
